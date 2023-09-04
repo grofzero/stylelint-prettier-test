6f10abad4b9d5f4c0017bb3bbb43bdbc42298b11
Horváth Kornél <kornel.horvath@shiwaforce.com>
2023-09-04 11:12:47 +0200

    stylelint prettier bug

---
+++ frontend/source/Test.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.test(aaa="ad" fff="aaaa") Test
+</template>
+<script setup>
+console.log('test');
+</script>
+<style lang="scss">
+.test {
+    background-color: orange
+}
+</style>
